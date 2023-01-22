5d9ce8dfb38f740075924f7dd80116d81d0184e3
NurSophy <sophy.burkert@gmail.com>
2023-01-22 12:38:08 +0100

    deleted comment-edit

--- Customerservice/urls.py
+++ Customerservice/urls.py
@@ -4,8 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
-    path('edit/<int:pk>/', views.CommentEditView.as_view(), name='comment-edit'),
-    path('editdelete/<int:pk>/', views.comment_edit_delete, name='comment-edit-delete'),
     path('home/', TemplateView.as_view(template_name='customerservice-home.html'), name='customerservice-home'),
     path('reported-delete/', views.ReportedDeleteView.as_view(), name='reported-comments-delete'),
 ]
